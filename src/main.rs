4555dfc52e23248b3981996bf6a6c67b18f7baf6
Tamae Honami <Tamae00Honami@gmail.com>
2020-02-21 16:46:11 +0900

    [Modify] Add "if" and "for" syntax examples.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,18 @@ fn main() {
     y + 1
   };
   println!("{}", x);
+
+  let condition: bool = true;
+  if condition {
+    println!("true");
+  } else {
+    println!("false");
+  }
+
+  let arr = [10, 20, 30, 40, 50];
+  for element in arr.iter() {
+    println!("{}", element);
+  }
 }
 
 fn add(x: i32, y: i32) -> i32 {
