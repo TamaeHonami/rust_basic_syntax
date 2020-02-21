bf2dd6ea0c11c8df1f4767aed1adb4e64150ca6f
Tamae Honami <Tamae00Honami@gmail.com>
2020-02-21 21:30:43 +0900

    [Modify] Add ownership example.

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,66 @@ fn main() {
   for element in arr.iter() {
     println!("{}", element);
   }
+
+  let test1: i32 = 5;
+  let test2: i32 = test1;
+  println!("{}", test1);
+  println!("{}", test2);
+
+  {
+    let s1 = String::from("hello");
+    println!("s1 {}", s1);
+    let s2 = s1;
+    println!("s2 {}", s2);
+    // One more s1 print NG!!
+    //println!("s1 {}", s1);
+  }
+
+  let s3: String = String::from("from");
+  let len: usize = get_length(&s3);
+  println!("{} {}", s3, len);
+
+  let mut s4: String = String::from("hello");
+  change(&mut s4);
+  println!("{}", s4);
+
+  let my_string: String = String::from("Hello world!");
+  let word: &str = first_word(&my_string[..]);
+  println!("{}", word);
+
+  let my_str_literal: &str = "Hello world!";
+  let word: &str = first_word(&my_str_literal[..]);
+  println!("{}", word);
+  // or
+  let word: &str = first_word(my_str_literal);
+  println!("{}", word);
+
+  let arr2: [i32; 5] = [1, 2, 3, 4, 5];
+  let arr_slice: &[i32] = &arr2[1..4];
+  for elem in arr_slice.iter() {
+    println!("{}", elem);
+  }
 }
 
 fn add(x: i32, y: i32) -> i32 {
   x + y
+}
+
+fn get_length(s: &String) -> usize {
+  s.len()
+}
+
+fn change(s: &mut String) {
+  s.push_str(", world!");
+}
+
+fn first_word(s: &str) -> &str {
+  let bytes = s.as_bytes();
+
+  for (i, &item) in bytes.iter().enumerate() {
+    if item == b' ' {
+      return &s[0..i]
+    }
+  }
+  &s[..]
 }
